a971bc9946e846fd4d9a49d09b10fecd7a8ad36c
Dragan Stancevic <draganstancevic15@gmail.com>
2022-07-02 22:11:24 +0200

    Users can edit profile

--- FitnessCenterApp/Controllers/UserController.cs
+++ FitnessCenterApp/Controllers/UserController.cs
@@ -19,12 +19,10 @@ namespace FitnessCenterApp.Controllers
         [HttpPost]
         public HttpResponseMessage LogIn(LoginModel login)
         {
-            List<User> users = new List<User>();
-            users.AddRange(WorkingWithFiles.ReadEntitiesFromFIle<Visitor>(visitorsPath));
-            users.AddRange(WorkingWithFiles.ReadEntitiesFromFIle<Owner>(ownersPath));
-            users.AddRange(WorkingWithFiles.ReadEntitiesFromFIle<Coach>(coachPath));
-            
-            foreach(User u in users)
+            List<User> users = GetAllUsersFromFile();
+
+
+            foreach (User u in users)
             {
                 if (login.Username.Equals(u.Username) && login.Password.Equals(u.Password))
                 {
@@ -49,6 +47,100 @@ namespace FitnessCenterApp.Controllers
             
         }
 
+        [HttpGet]
+        public string GetUser(string username)
+        {
+            List<User> users = GetAllUsersFromFile();
+            User user = users.FirstOrDefault(x => x.Username.Equals(username));
+
+            if(user == null)
+            {
+                return JsonSerializer.Serialize("");
+            }
+
+            return JsonSerializer.Serialize(user);
+        }
+
+        [HttpPut]
+        public HttpResponseMessage UpdateUser(Visitor newUser)
+        {
+            if(newUser.UserRole == Role.VISITOR)
+            {
+                List<Visitor> visitors = WorkingWithFiles.ReadEntitiesFromFIle<Visitor>(visitorsPath);
+                Visitor oldUser = visitors.FirstOrDefault(x => x.Username.Equals(newUser.Username));
+                if(oldUser == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "User not found in database");
+                }
+
+                oldUser.Name = newUser.Name;
+                oldUser.Lastname = newUser.Lastname;
+                oldUser.Password = newUser.Password;
+                oldUser.UserGender = newUser.UserGender;
+                oldUser.Birth = newUser.Birth;
+                oldUser.Email = newUser.Email;
+
+                WorkingWithFiles.RewriteFileWithEntities<Visitor>(visitors, visitorsPath);
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else if (newUser.UserRole == Role.COACH)
+            {
+                List<Coach> coaches = WorkingWithFiles.ReadEntitiesFromFIle<Coach>(coachPath);
+                Coach oldUser = coaches.FirstOrDefault(x => x.Username.Equals(newUser.Username));
+                if (oldUser == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "User not found in database");
+                }
+
+                oldUser.Name = newUser.Name;
+                oldUser.Lastname = newUser.Lastname;
+                oldUser.Password = newUser.Password;
+                oldUser.UserGender = newUser.UserGender;
+                oldUser.Birth = newUser.Birth;
+                oldUser.Email = newUser.Email;
+
+                WorkingWithFiles.RewriteFileWithEntities<Coach>(coaches, coachPath);
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else if (newUser.UserRole == Role.OWNER)
+            {
+                List<Owner> owners = WorkingWithFiles.ReadEntitiesFromFIle<Owner>(ownersPath);
+                Owner oldUser = owners.FirstOrDefault(x => x.Username.Equals(newUser.Username));
+                if (oldUser == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "User not found in database");
+                }
+
+                oldUser.Name = newUser.Name;
+                oldUser.Lastname = newUser.Lastname;
+                oldUser.Password = newUser.Password;
+                oldUser.UserGender = newUser.UserGender;
+                oldUser.Birth = newUser.Birth;
+                oldUser.Email = newUser.Email;
+
+                WorkingWithFiles.RewriteFileWithEntities<Owner>(owners, ownersPath);
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Error in server");
+            }
+
+        }
+
+      
+
+        private List<User> GetAllUsersFromFile()
+        {
+            List<User> users = new List<User>();
+            users.AddRange(WorkingWithFiles.ReadEntitiesFromFIle<Visitor>(visitorsPath));
+            users.AddRange(WorkingWithFiles.ReadEntitiesFromFIle<Owner>(ownersPath));
+            users.AddRange(WorkingWithFiles.ReadEntitiesFromFIle<Coach>(coachPath));
+            return users;
+        }
 
         private Coach GetCoach(string username)
         {
--- FitnessCenterApp/Scripts/profile.js
+++ FitnessCenterApp/Scripts/profile.js
@@ -1,7 +1,58 @@
+var CurrentUser
+
+$(document).ready(function () {
+    if(localStorage.LoggedInUser == '' || localStorage.LoggedInUser == null){
+        return
+    }
+    let username = localStorage.LoggedInUser.split('_')[1]
+
+    $.ajax({
+        type: "GET",
+        url: "../api/User?username=" + username,
+        data: "",
+        dataType: "json",
+        complete: function (response) {
+            CurrentUser = JSON.parse(response.responseJSON)
+            let gender = 'MALE'
+            let role = 'VISITOR'
+
+            if(CurrentUser.UserGender == 1){
+                gender = 'FEMALE'
+            }
+
+            if(CurrentUser.UserRole == 1){
+                role = 'COACH'
+            }
+            else if(CurrentUser.UserRole == 2){
+                role = 'OWNER'
+            }
+
+            $('#my_username').text(CurrentUser.Username)
+            $('#my_name').text(CurrentUser.Name)
+            $('#my_lastname').text(CurrentUser.Lastname)
+            $('#my_gender').text(gender)
+            $('#my_email').text(CurrentUser.Email)
+            $('#my_birth').text(CurrentUser.Birth.split('T')[0])
+            $('#my_role').text(role)
+           
+        }
+    });
+});
+
+
+
 $('#edit_profile_btn').click(function (e) { 
     $('#edit_div').show()
     $('#cancel_profile_btn').show()
     $('#edit_profile_btn').hide()
+
+
+    $('#edit_name').val(CurrentUser.Name)
+    $('#edit_lastname').val(CurrentUser.Lastname)
+    $('#edit_gender').val(CurrentUser.UserGender).change()
+    $('#edit_email').val(CurrentUser.Email)
+    $('#edit_birth').val(CurrentUser.Birth.split('T')[0])
+    
     
 });
 
@@ -10,4 +61,66 @@ $('#cancel_profile_btn').click(function (e) {
     $('#edit_div').hide()
     $('#cancel_profile_btn').hide()
     $('#edit_profile_btn').show()
+});
+
+$('#edit_profile_form').submit(function (e) { 
+    e.preventDefault()
+    let name = $('#edit_name').val()
+    let lastname = $('#edit_lastname').val()
+    let gender = $('#edit_gender').val()
+    let email = $('#edit_email').val()
+    let birth = $('#edit_birth').val()
+    let oldPassword = $('#edit_password').val()
+    let newPassword = $('#edit_new_password').val()
+
+
+    if(name == '' || lastname == '' || email == '' || birth == '' || oldPassword == ''){
+        alert('Some data are empty')
+        return false
+    }
+
+    if(CurrentUser.Password != oldPassword){
+        alert("Wrong password")
+        $('#edit_password').focus()
+        return false
+    }
+
+    let updatePassword = newPassword
+    if(newPassword == ''){
+        updatePassword = oldPassword
+    }
+
+    // public string Username { get; set; }
+    //     public string Password { get; set; }
+    //     public string Name { get; set; }
+    //     public string Lastname { get; set; }
+    //     public Gender UserGender { get; set; }
+    //     public string Email { get; set; }
+    //     public DateTime Birth { get; set; }
+    //     public Role UserRole { get; set; }
+
+    let query = {Username:CurrentUser.Username, Name:name, Lastname:lastname, Password:updatePassword,
+                Email:email, UserGender:gender, Birth:birth, UserRole:CurrentUser.UserRole}
+
+    query = JSON.stringify(query)
+
+    console.log(query)
+    $.ajax({
+        type: "PUT",
+        url: "../api/User",
+        data: query,
+        dataType: "json",
+        contentType: "application/json",
+        complete: function (response) {
+            if(response.status != 200){
+                alert(response.responseText)
+                
+            }
+            else{
+                location.reload()
+            }
+           
+        }
+    });
+    
 });
